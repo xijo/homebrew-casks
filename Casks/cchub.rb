f232856be6075c0dd7bd5fc7b0f12c8b5a3b167f
Johannes Opper <xijo@pm.me>
2021-09-07 20:58:50 +0200

    Update cchub to 0.2.0

--- Casks/cchub.rb
+++ Casks/cchub.rb
@@ -1,6 +1,6 @@
 cask "cchub" do
-  version "0.1.1"
-  sha256 "3aa4206581ce54340d8d3f50268e7a79fb74aff07f0f53aee5a3ced85c94ad26"
+  version "0.2.0"
+  sha256 "c5bf36a9bc5848272e6a39cd3f2af8dc2a35d31590f0f1117a6f1fe9ef06730d"
 
   url "https://github.com/xijo/cchub/releases/download/v#{version}/cchub-#{version}.dmg",
       verified: "github.com/xijo/cchub/"
